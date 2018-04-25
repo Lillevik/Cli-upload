00fee819148cd7b824ec257702aa7ddbccd5ab28
Marius Lillevik <mariusfalch@hotmail.com>
2018-04-25 13:00:45 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -1,57 +1,20 @@
-from subprocess import call
-from setuptools import Command, find_packages, setup
+from setuptools import find_packages, setup
 from upl import __version__
-
-
-class RunTests(Command):
-    """Run all tests."""
-    description = 'run tests'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """Run all tests!"""
-        errno = call(['py.test', '--cov=upl', '--cov-report=term-missing'])
-        raise SystemExit(errno)
-
 
 setup(
     name='upl',
     version=__version__,
-    description='A file upload command line program in Python.',
-    url='',
+    description='A file upload command line program.',
+    url='http://github.com/Lillevik/Cli-upload',
     author='Marius Lillevik',
-    author_email='',
-    license='UNLICENSE',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: Public Domain',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ],
+    license='GNU',
     keywords='cli',
-    packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=['docopt', 'requests'],
-    extras_require={
-        'test': ['coverage', 'pytest', 'pytest-cov'],
-    },
+    packages=find_packages(),
+    zip_safe=False,
     entry_points={
         'console_scripts': [
             'upl=upl.cli:main',
         ],
-    },
-    cmdclass={'test': RunTests},
+    }
 )
