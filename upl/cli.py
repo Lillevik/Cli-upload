be8c4d24155ffa88b51ff449bb1816d0e2ded6fe
Marius Lillevik <mariusfalch@hotmail.com>
2018-04-25 12:30:21 +0200

    Moved import and better error handling

--- upl/cli.py
+++ upl/cli.py
@@ -17,28 +17,33 @@ Examples:
 
 from docopt import docopt
 from upl import __version__ as VERSION
+import os, requests
 
 
 def main():
     """Main CLI entrypoint."""
     options = docopt(__doc__, version=VERSION)
     if '<file>' in options:
-        import os, requests
         cwd = os.getcwd()
         filepath = os.path.join(cwd, options['<file>'])
-        if os.path.isfile(filepath):
+        try:
             in_file = open(filepath, 'rb')
             print('Uploading ' + options['<file>'])
-            try:
-                r = requests.post('https://i.lillevik.pw/upload/file', files={'file': in_file})
-                if r.status_code == 200:
-                    print('Done! url: ' + r.json()['location'])
-                elif r.status_code == 413:
-                    print('Error: file too large. Max 100mb.')
-                else:
-                    print('Error: An error occurred during upload.')
-            except Exception:
-                print('Error: An unexpected error occurred.')
-        else:
+            r = requests.post('https://i.lillevik.pw/upload/file', files={'file': in_file})
+            if r.status_code == 200:
+                print('Done! url: ' + r.json()['location'])
+            elif r.status_code == 413:
+                print('Error: file too large. Max 100mb.')
+            else:
+                print('Error: An error occurred during upload.')
+        except FileNotFoundError:
             print('Error: Could not find the file.')
+        except PermissionError:
+            print('Error: Insufficient permissions.')
+        except IsADirectoryError:
+            print('Error: File is a directory.')
+        except Exception:
+            print('Error: An unexpected error occurred. Please create an issue on github. ')
+
+
 
